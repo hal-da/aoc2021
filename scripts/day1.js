2246307823e79ee2a5403d6fd294f3ae77f17ab5
Daniel Halasz <daniel.halasz@gmx.net>
2021-12-01 09:04:40 +0100

    day 1

--- scripts/day1.js
+++ scripts/day1.js
@@ -1,5 +1,21 @@
-import {data} from "./day1data.js";
-
+import {data1A, testData} from "./day1data.js";
 console.log('hallo')
-console.log(data.data)
-console.log(data)
+const measurements = data1A.trim().split('\n').map(Number)
+//console.log(measurements)
+
+let measureIncrementsA = 0;
+
+for(let i = 0; i < measurements.length; i++){
+    if(measurements[i] > measurements[i-1] ) {
+        measureIncrementsA++
+    }
+}
+console.log(measureIncrementsA)
+
+let measureIncrementsB = 0;
+for(let i = 0; i < measurements.length; i++){
+    if(measurements[i]+measurements[i+1]+measurements[i+2] > measurements[i-1]+measurements[i]+measurements[i+1] ) {
+        measureIncrementsB++
+    }
+}
+console.log(measureIncrementsB, 'B')
