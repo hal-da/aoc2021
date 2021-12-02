45efc47608eb399e683a63e6a872ea4164d83940
Daniel Halasz <daniel.halasz@gmx.net>
2021-12-02 06:35:13 +0100

    day 2 - crowbar

--- scripts/day1.js
+++ scripts/day1.js
@@ -1,21 +1,17 @@
 import {data1A, testData} from "./day1data.js";
-console.log('hallo')
+console.log('hallo 1')
 const measurements = data1A.trim().split('\n').map(Number)
 //console.log(measurements)
 
 let measureIncrementsA = 0;
 
 for(let i = 0; i < measurements.length; i++){
-    if(measurements[i] > measurements[i-1] ) {
-        measureIncrementsA++
-    }
+    measureIncrementsA += (measurements[i] > measurements[i-1] )
 }
 console.log(measureIncrementsA)
 
 let measureIncrementsB = 0;
-for(let i = 0; i < measurements.length; i++){
-    if(measurements[i]+measurements[i+1]+measurements[i+2] > measurements[i-1]+measurements[i]+measurements[i+1] ) {
-        measureIncrementsB++
-    }
+for(let i = 0; i < measurements.length +20 ; i++){
+    measureIncrementsB += (measurements[i]+measurements[i+1]+measurements[i+2] > measurements[i-1]+measurements[i]+measurements[i+1])
 }
 console.log(measureIncrementsB, 'B')
---
+++ scripts/day2.js
@@ -0,0 +1,50 @@
+console.log('day2')
+import {data, testData} from "./day2data.js";
+
+const dataArr = data.split('\n')
+console.log(dataArr)
+
+const position = {
+    horizontal: 0,
+    depth:0,
+}
+dataArr.forEach(data => {
+    const direction = data.split(' ');
+    switch (direction[0]){
+        case 'forward': position.horizontal += direction[1]*1
+            break
+        case 'down':position.depth += direction[1]*1
+            break
+        case 'up':position.depth -= direction[1]*1
+            break
+        default:
+            console.log('should not happen')
+    }
+})
+console.log(position)
+console.log(position.depth * position.horizontal)
+// 2
+const positionAim = {
+    horizontal: 0,
+    depth:0,
+    aim:0,
+}
+dataArr.forEach(data => {
+    const direction = data.split(' ');
+    switch (direction[0]){
+        case 'forward':
+            positionAim.horizontal += direction[1]*1
+            positionAim.depth += positionAim.aim*direction[1]
+            break
+        case 'down':positionAim.aim += direction[1]*1
+            break
+        case 'up':positionAim.aim -= direction[1]*1
+            break
+        default:
+            console.log('should not happen')
+    }
+})
+
+
+console.log(positionAim)
+console.log(positionAim.depth * positionAim.horizontal)
