135aebc6593f68ab405532c1c152218a26b87e95
Daniel Halasz <daniel.halasz@gmx.net>
2021-12-06 08:09:25 +0100

    day6 what a treat. cool. calm. collected. even better.

--- scripts/day6/day6.js
+++ scripts/day6/day6.js
@@ -1,40 +1,33 @@
 import {realData, testData} from "./day6data.js";
-
 let data = realData.split(',').map(Number)
+let days = 256
 console.log('day6')
+// part one and two
+let schwarm = Array(9).fill(0), copy = []
 
-// part one
-console.log(data,  'first data')
-function part1(){
-    let zeros = 0, newSpawns = [], oldFisch = [], allZeros = 0
-    for (let i = 0; i < 80; i++) {
-        data = data.map(x => --x)
-        allZeros = data.filter(x => x === 0).length
-        data = [...data,...Array(allZeros).fill(7),...Array(allZeros).fill(9)].filter(x => x > 0)
-    }
-}
-
-// part two
-
-let schwarm = []
-for (let i = 0; i < 9; i++) {
-    schwarm[i] = 0
-}
 // fill first fish school
 data.forEach( fish => schwarm[fish]++)
 
-let copy = []
-
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < days; i++) {
     for (let j = 0; j < schwarm.length-1; j++) {
         copy[j] = schwarm[j+1]
     }
     copy[6] += schwarm[0]
     copy[8] = schwarm[0]
-    for (let j = 0; j < schwarm.length; j++) {
-        schwarm[j] = copy[j]
-    }
+    schwarm = [...copy]
 }
 let sum = 0
 schwarm.forEach( x => sum += x)
 console.log(sum)
+
+//part one - first attempt
+// console.log(data,  'first data')
+function part1(){
+    let allZeros = 0
+    for (let i = 0; i < 80; i++) {
+        data = data.map(x => --x)
+        allZeros = data.filter(x => x === 0).length
+        data = [...data,...Array(allZeros).fill(7),...Array(allZeros).fill(9)].filter(x => x > 0)
+    }
+    console.log(data.length)
+}
