4b9f573ac7c19e6131fa87c182a4185a760b1ab4
Daniel Halasz <daniel.halasz@gmx.net>
2021-12-06 08:13:53 +0100

    day6 what a treat. cool. calm. collected. even better. first attempt in old commit

--- scripts/day6/day6.js
+++ scripts/day6/day6.js
@@ -3,31 +3,19 @@ let data = realData.split(',').map(Number)
 let days = 256
 console.log('day6')
 // part one and two
-let schwarm = Array(9).fill(0), copy = []
+let swarm = Array(9).fill(0), swarmCopy = []
 
 // fill first fish school
-data.forEach( fish => schwarm[fish]++)
+data.forEach( fish => swarm[fish]++)
 
 for (let i = 0; i < days; i++) {
-    for (let j = 0; j < schwarm.length-1; j++) {
-        copy[j] = schwarm[j+1]
+    for (let j = 0; j < swarm.length-1; j++) {
+        swarmCopy[j] = swarm[j+1]
     }
-    copy[6] += schwarm[0]
-    copy[8] = schwarm[0]
-    schwarm = [...copy]
+    swarmCopy[6] += swarm[0]
+    swarmCopy[8] = swarm[0]
+    swarm = [...swarmCopy]
 }
 let sum = 0
-schwarm.forEach( x => sum += x)
+swarm.forEach( x => sum += x)
 console.log(sum)
-
-//part one - first attempt
-// console.log(data,  'first data')
-function part1(){
-    let allZeros = 0
-    for (let i = 0; i < 80; i++) {
-        data = data.map(x => --x)
-        allZeros = data.filter(x => x === 0).length
-        data = [...data,...Array(allZeros).fill(7),...Array(allZeros).fill(9)].filter(x => x > 0)
-    }
-    console.log(data.length)
-}
