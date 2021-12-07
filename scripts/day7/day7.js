3ab771819d8546c4acfcaf8918ff82a63dd1f270
Daniel Halasz <daniel.halasz@gmx.net>
2021-12-07 14:37:05 +0100

    day7 - that's ridiculous

--- scripts/day7/day7.js
+++ scripts/day7/day7.js
@@ -6,9 +6,7 @@ let part2 = true
 
 for (let i = 0; i < data[data.length-1]; i++) {
     for (let j = 0; j < data.length; j++) {
-        let diff =  Math.abs(data[j]-i)
-        if( part2 ) diff = (diff * (diff+1))/2
-        answer[i] += diff
+        answer[i] += part2 ? Math.abs(((data[j]-i) * (data[j]-i)+1)/2) : data[j]-i
     }
 }
 console.log(Math.min(...answer))
