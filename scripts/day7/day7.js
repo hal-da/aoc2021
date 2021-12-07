3a1950a26b23888aa42030b7375cca960432a278
Daniel Halasz <daniel.halasz@gmx.net>
2021-12-07 14:45:52 +0100

    day7 - final answer

--- scripts/day7/day7.js
+++ scripts/day7/day7.js
@@ -1,12 +1,13 @@
 console.log('day7')
 import {testData, realData} from "./day7data.js";
-let data = realData.split(',').sort((a,b)=>a-b).map(Number)
+let data = realData.split(',').map(Number)
 let answer = Array(data[data.length-1]).fill(0)
 let part2 = true
 
 for (let i = 0; i < data[data.length-1]; i++) {
     for (let j = 0; j < data.length; j++) {
-        answer[i] += part2 ? Math.abs(((data[j]-i) * (data[j]-i)+1)/2) : data[j]-i
+        let diff =  Math.abs(data[j]-i)
+        answer[i] += part2 ? (diff * diff+1)/2 :diff
     }
 }
 console.log(Math.min(...answer))
