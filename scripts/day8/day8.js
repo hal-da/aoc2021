1dda489e6563420c61af23c793c13091aefc7c70
Daniel Halasz <daniel.halasz@gmx.net>
2021-12-08 14:18:00 +0100

    day8 - finally - aufgem tut ma nur an brief

--- scripts/day8/day8.js
+++ scripts/day8/day8.js
@@ -1,4 +1,4 @@
-console.log('day8')
+console.log('day8c')
 import {realData, testData, testData2} from "./day8data.js";
 let data = realData.split('\n')
 let alphabet, allDigits = [], forDigits= [], sum = 0
@@ -21,18 +21,18 @@ console.log(sum)
 //NOT 1060214
 function getAlphabet(i){
     alphabet = {
-        0:'aaaaaaaaaaaaa',
-        1:'aaaaaaaaaaaaa',
-        2:'aaaaaaaaaaaaa',
-        3:'aaaaaaaaaaaaa',
-        4:'aaaaaaaaaaaaa',
-        5:'aaaaaaaaaaaaa',
-        6:'aaaaaaaaaaaaa',
-        7:'aaaaaaaaaaaaa',
-        8:'aaaaaaaaaaaaa',
-        9:'aaaaaaaaaaaaa',
+        0:'xxxxxxxxxxxxxx',
+        1:'xxxxxxxxxxxxxx',
+        2:'xxxxxxxxxxxxxx',
+        3:'xxxxxxxxxxxxxx',
+        4:'xxxxxxxxxxxxxx',
+        5:'xxxxxxxxxxxxxx',
+        6:'xxxxxxxxxxxxxx',
+        7:'xxxxxxxxxxxxxx',
+        8:'xxxxxxxxxxxxxx',
+        9:'xxxxxxxxxxxxxx',
     }
-    allDigits = data[i].split(' ').sort((a,b)=> a.length - b.length).map(x => x.split('').sort().join(''))
+    allDigits = data[i].split(' ').sort((a,b)=> a.length - b.length).map(x => x.split('').sort().join('').trim())
     for (let j = 0; j < 5; j++) {
         allDigits.forEach(s => {
             switch (s.length){
@@ -48,26 +48,10 @@ function getAlphabet(i){
                 case 7:alphabet[s]=8
                     alphabet[8]=s
                     break
-                case 6:{
-                    if(!(s.includes(alphabet[3][0])&& s.includes(alphabet[3][1]) && s.includes(alphabet[3][2]) && s.includes(alphabet[3][3]) && s.includes(alphabet[3][4])) &&
-                        (s.includes(alphabet[4][0]) &&  s.includes(alphabet[4][1]) &&  s.includes(alphabet[4][2]) &&  s.includes(alphabet[4][3])) ) {
-                        alphabet[s]=6
-                        alphabet[6]=s
-                    } else if ((s.includes(alphabet[3][0])&& s.includes(alphabet[3][1]) && s.includes(alphabet[3][2]) && s.includes(alphabet[3][3]) && s.includes(alphabet[3][4])) ||
-                        (s.includes(alphabet[4][0]) &&  s.includes(alphabet[4][1]) &&  s.includes(alphabet[4][2]) &&  s.includes(alphabet[4][3]))){
-                        alphabet[s]=9
-                        alphabet[9]=s
-                    } else if ((!s.includes(alphabet[3][0])&& s.includes(alphabet[3][1]) && s.includes(alphabet[3][2]) && s.includes(alphabet[3][3]) && s.includes(alphabet[3][4])) &&
-                        !(s.includes(alphabet[4][0]) &&  s.includes(alphabet[4][1]) &&  s.includes(alphabet[4][2]) &&  s.includes(alphabet[4][3]))){
-                        alphabet[0]=s
-                        alphabet[s]=0
-                    } else {
-                        alphabet[s]=6
-                        alphabet[6]=s
-                    }
-                } break
-                case 5: {
-                    if((s.includes(alphabet[7][0]) && s.includes(alphabet[7][1])&& s.includes(alphabet[7][2]))||(s.includes(alphabet[1][0]&&s.includes(alphabet[1][1]))) ){
+                case 5:
+                    if(s.includes(alphabet[1][0]) && s.includes(alphabet[1][1])
+                        || s.includes(alphabet[7][0]) && s.includes(alphabet[7][1]) && s.includes(alphabet[7][2])
+                    ) {
                         alphabet[3]=s
                         alphabet[s]=3
                         break
@@ -87,20 +71,26 @@ function getAlphabet(i){
                         alphabet[2]=s
                         alphabet[s]=2
                     }
-                    // console.log(s, x9, x4)
-                    // console.log(JSON.stringify(alphabet[9]))
-                    // console.log(JSON.stringify(alphabet[4]))
-                    // if(x9===5){
-                    //     alphabet[5]=s
-                    //     alphabet[s]=5
-                    // } else if(x===4){
-                    //     alphabet[2]=s
-                    //     alphabet[s]=2
-                    // }
-                    // console.log(JSON.stringify(alphabet))
-                } break
+                    break
+                case 6:
+                    if(!(s.includes(alphabet[1][0]) && s.includes(alphabet[1][1]))
+                        || !(s.includes(alphabet[7][0]) && s.includes(alphabet[7][1]) && s.includes(alphabet[7][2]))
+
+                    ) {
+                        alphabet[6]=s
+                        alphabet[s]=6
+                        break
+                    }
+                    if(s.includes(alphabet[4][0]) && s.includes(alphabet[4][1]) && s.includes(alphabet[4][2]) && s.includes(alphabet[4][3])){
+                        alphabet[9]=s
+                        alphabet[s]=9
+                    } else if(!(s.includes(alphabet[4][0]) && s.includes(alphabet[4][1]) && s.includes(alphabet[4][2]) && s.includes(alphabet[4][3]))) {
+                        alphabet[0]=s
+                        alphabet[s]=0
+                    }
+
+                    break
             }
-            // console.log(alphabet)
         })
 
     }
@@ -133,3 +123,22 @@ function part1(){
 //     abcdefg:8,
 //     abcdfg:8
 // }
+
+/*
+if(!(s.includes(alphabet[3][0])&& s.includes(alphabet[3][1]) && s.includes(alphabet[3][2]) && s.includes(alphabet[3][3]) && s.includes(alphabet[3][4])) &&
+                        (s.includes(alphabet[4][0]) &&  s.includes(alphabet[4][1]) &&  s.includes(alphabet[4][2]) &&  s.includes(alphabet[4][3])) ) {
+                        alphabet[s]=6
+                        alphabet[6]=s
+                    } else if ((s.includes(alphabet[3][0])&& s.includes(alphabet[3][1]) && s.includes(alphabet[3][2]) && s.includes(alphabet[3][3]) && s.includes(alphabet[3][4])) ||
+                        (s.includes(alphabet[4][0]) &&  s.includes(alphabet[4][1]) &&  s.includes(alphabet[4][2]) &&  s.includes(alphabet[4][3]))){
+                        alphabet[s]=9
+                        alphabet[9]=s
+                    } else if ((!s.includes(alphabet[3][0])&& s.includes(alphabet[3][1]) && s.includes(alphabet[3][2]) && s.includes(alphabet[3][3]) && s.includes(alphabet[3][4])) &&
+                        !(s.includes(alphabet[4][0]) &&  s.includes(alphabet[4][1]) &&  s.includes(alphabet[4][2]) &&  s.includes(alphabet[4][3]))){
+                        alphabet[0]=s
+                        alphabet[s]=0
+                    } else {
+                        alphabet[s]=6
+                        alphabet[6]=s
+                    }
+ */
