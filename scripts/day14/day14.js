a12d419b1176e55775d8fa069accfdc5098140de
Daniel Halasz <daniel.halasz@gmx.net>
2021-12-14 16:31:07 +0100

    day14 unfertig

--- scripts/day14/day14.js
+++ scripts/day14/day14.js
@@ -1,23 +1,105 @@
 console.log('day14')
 import {realData, testData}  from "./day14data.js";
 let myTempPolymer = [], dictionary = {}, myCount = {}
-let polymer = realData.polymer
-let insertions = realData.insertions.split('\n').map(x=> x.split(' -> ').join('').split(''))
+let polymer = testData.polymer
+let insertions = testData.insertions.split('\n').map(x=> x.split(' -> ').join('').split(''))
 let steps = 10
 
-createDictionary()
-
-for (let i = 0; i < steps; i++) {
-
-    fillTempPolymer()
-    substituteTmpPolymer()
+for (let i = 0; i < polymer.length; i++) {
+    if(myCount[polymer[i]]) myCount[polymer[i]]++
+    else myCount[polymer[i]] = 1
 }
-countEmAll()
-console.log(myCount)
+createDictPart2()
+part2()
 
-console.log('polymer.length: ', polymer.length)
-// console.log(myTempPolymer)
-console.log(dictionary)
+
+function part2(){
+    for (let i = 0; i < steps; i++) {
+        letsInsert()
+    }
+
+    // for (let i = 0; i < steps; i++) {
+    //     createDictPart2(polymer)
+    //
+    //     applyInsert(polymer)
+    //     insertions.forEach(insert => applyInsert(insert))
+    //     countP2()
+    // }
+
+
+    console.log('myCount', myCount)
+    console.log('dictionary',dictionary)
+    console.log('insertions', insertions)
+}
+
+
+function letsInsert(){
+    let toDelete = [], toCreate = []
+    insertions.forEach(insert => {
+        if(dictionary[insert[0]+''+insert[1]]) {
+
+            toDelete.push(insert[0]+''+insert[1])
+            toCreate.push(insert[0]+'' +insert[2])
+            toCreate.push(insert[2]+'' +insert[1])
+            //
+            //
+            // delete dictionary[insert[0]+''+insert[1]]
+            // if(!dictionary[insert[0]+'' +insert[2]])dictionary[insert[0]+'' +insert[2]] =1
+            // if(!dictionary[insert[2]+'' +insert[1]])dictionary[insert[2]+'' +insert[1]] =1
+        }
+    })
+    deleteAndCreateDict(toDelete, toCreate)
+    insertions.forEach(insert => {
+        if (dictionary[insert[0] + '' + insert[1]]) {
+            if (myCount[insert[2]]) myCount[insert[2]]++
+            else myCount[insert[2]] = 1
+        }
+    })
+
+}
+
+function deleteAndCreateDict(toDelete, toCreate){
+    toDelete.forEach(d => {
+        delete dictionary[d]
+    })
+
+    toCreate.forEach(t => {
+        if(!dictionary[t]) dictionary[t] =1
+    })
+
+}
+
+
+
+
+function createDictPart2(){
+    for (let i = 0; i < polymer.length-1; i++) {
+        dictionary[polymer[i]+''+polymer[i+1]] = 1
+    }
+}
+
+
+
+function part1(){
+    createDictionary()
+
+    for (let i = 0; i < steps; i++) {
+        fillTempPolymer()
+        substituteTmpPolymer()
+    }
+    countEmAll()
+    console.log(myCount)
+}
+
+// console.log('done')
+//
+// // console.log(polymer)
+// // countEmAll()
+// console.log(myCount)
+//
+// console.log('polymer.length: ', polymer.length)
+// // console.log(myTempPolymer)
+// console.log(dictionary)
 
 function countEmAll(){
     for (let i = 0; i < polymer.length; i++) {
@@ -26,6 +108,8 @@ function countEmAll(){
         else myCount[polymer[i]]=1
     }
 }
+
+
 
 function substituteTmpPolymer(){
     for (let i = 0; i < myTempPolymer.length; i++) {
